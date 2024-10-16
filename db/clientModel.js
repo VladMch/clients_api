c514e0e83ef8516f63c7640c8b74d361246ba24e
ukrai <wot2107@gmail.com>
2024-10-16 18:08:12 +0300

    add model, modify connectDB, added mongodb to import

---
+++ db/clientModel.js
@@ -0,0 +1,10 @@
+import { Schema, model } from 'mongoose';
+
+const clientSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  count: { type: Number, required: true, min: 0 },
+  expDate: Date
+});
+
+const CLient = model('CLient', clientSchema);
+export default CLient;
---
+++ db/connectDB.js
@@ -0,0 +1,41 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+const dbUrl = process.env.DATABASE_URL;
+
+// mongoose.connect(dbUrl, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => {
+//   console.log('Успешное подключение к базе данных');
+// }).catch((error) => {
+//   console.error('Ошибка подключения к базе данных:', error);
+// });
+
+// mongoose.connection.on('connected', () => {
+//   console.log('Подключено к базе данных MongoDB');
+// });
+
+
+//mongo URI
+const client = new MongoClient(dbUrl, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  }
+  finally {
+
+  }
+}
+
+run().catch(error => console.log)
